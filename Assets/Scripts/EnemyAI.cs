6c6963dbbdede6f211b8df038c53c3195d0200a1
Niko Novak <novakniko2000@gmail.com>
2024-06-05 15:06:00 +0200

    Download all necessary packages

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.InputSystem.Android.LowLevel;
+using UnityEngine.InputSystem;
 
 public class EnemyAI : MonoBehaviour
 {
